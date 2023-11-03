da873d43fb0c5908f1b10945f198ea554b84d7bf
Victorbarros081 <ead295vitinho@gmail.com>
2023-11-03 11:56:28 -0300

    Initial commit

---
+++ Heranca.py
@@ -0,0 +1,23 @@
+from Bibliotecaher import*
+
+gato=Gato('Pitoco', 'Preto e Branco' )
+gato.emitirsom()
+gato.comer()
+print()
+
+cachorro=Cachorro('shiuaua', 'caramelo')
+cachorro.emitirsom()
+cachorro.comer()
+print()
+
+coelho=Coelho('Pula Pula', 'Petro')
+coelho.emitirsom()
+coelho.comer()
+print()
+
+vaca=Vaca('Leitera', 'Preta e branca')
+vaca.emitirsom()
+vaca.comer()
+print()
+
+
